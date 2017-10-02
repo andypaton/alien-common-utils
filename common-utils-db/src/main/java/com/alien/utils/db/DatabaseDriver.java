d6f81f06d80ca10a5e2416f3845c237ab7f89497
Paton, Andrew <andrew.paton@sky.uk>
2017-10-02 08:23:36 +0100

    new classes for bd utils

---
+++ common-utils-db/src/main/java/com/alien/utils/db/DatabaseDriver.java
@@ -0,0 +1,19 @@
+package com.alien.utils.db;
+
+import java.sql.Connection;
+
+import javax.sql.DataSource;
+
+public interface DatabaseDriver {
+	
+    void cleanUp();
+    Connection createConnectionFor(String username, String password);
+    DataSource createDataSourceFor(String username, String password);
+    void createUser(String username, String password);
+    void dropUser(String username);
+    void dropUserQuietly(String username);
+    boolean hasTable(String esername, String tableName);
+    void truncateTable(String username, String tableName);
+    boolean userExists(String username);
+
+}
---
+++ common-utils-db/src/main/java/com/alien/utils/db/TearDownHandler.java
@@ -0,0 +1,7 @@
+package com.alien.utils.db;
+
+public interface TearDownHandler {
+
+    void tearDown(DatabaseDriver databaseDriver, String username);
+
+}
