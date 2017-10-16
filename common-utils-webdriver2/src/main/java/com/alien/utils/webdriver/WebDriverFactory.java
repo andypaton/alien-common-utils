c2776061d3f6273490cf80d8b0893730e914fb8c
andypaton <andypaton@macpro>
2017-10-16 20:41:32 +0200

    logging added

--- common-utils-webdriver2/src/main/java/com/alien/utils/webdriver/WebDriverFactory.java
+++ common-utils-webdriver2/src/main/java/com/alien/utils/webdriver/WebDriverFactory.java
@@ -1,11 +1,7 @@
 package com.alien.utils.webdriver;
 
-import static org.apache.commons.io.FileUtils.writeByteArrayToFile;
-import static org.apache.commons.io.IOUtils.toByteArray;
-
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
@@ -17,8 +13,13 @@ import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriverService;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 public final class WebDriverFactory {
+	
+    private static final Logger LOGGER = LoggerFactory.getLogger(WebDriverFactory.class);
 
     private static final int WAIT = 30;
 
@@ -28,20 +29,19 @@ public final class WebDriverFactory {
     private static final String GECKO_DRIVER_PATH = "./drivers/geckodriver";
     private static final String PHANTOMJS_DRIVER_PATH = "./drivers/phantom_driver/2_1_1/phantomjs";
 
-    private static final String HTMLUNIT = "htmlunit";
     private static final String FIREFOX = "firefox";
     private static final String CHROME = "chrome";
     private static final String PHANTOMJS = "phantom";
-
     
     private static String webDriver;
-    private static String hub;
 
 
     public static WebDriver getWebDriver() throws IOException{
     	
         if (System.getProperty("web.driver") != null) {
             webDriver = System.getProperty("web.driver");
+            
+            LOGGER.debug("WebDriver set to : " + webDriver);
         }
                 
         if (WEB_DRIVER == null) {
@@ -80,7 +80,9 @@ public final class WebDriverFactory {
 
     	ClassLoader classLoader = WebDriver.class.getClassLoader();
     	String path = new File(classLoader.getResource(file).getFile()).getAbsolutePath();
-
+    	
+    	LOGGER.debug("Path to WebDriver : " + path);
+    	
     	return path;
     }
     
@@ -90,8 +92,9 @@ public final class WebDriverFactory {
         if (chromeDriverFile.exists() && !chromeDriverFile.canExecute()) {
             chromeDriverFile.setExecutable(true);
         }
-    	
+    	        
         System.setProperty("webdriver.chrome.driver", getAbsolutePath(CHROME_DRIVER_PATH));
+        
         ChromeOptions options = new ChromeOptions();
         options.addArguments("start-maximized");
         DesiredCapabilities capabilities = DesiredCapabilities.chrome();
@@ -103,38 +106,38 @@ public final class WebDriverFactory {
     	
         System.setProperty("webdriver.gecko.driver", getAbsolutePath(GECKO_DRIVER_PATH));
 
-    	FirefoxProfile profile = new FirefoxProfile();
-    	profile.setAlwaysLoadNoFocusLib(true);
-    	return new FirefoxDriver(profile);
+    	    FirefoxProfile profile = new FirefoxProfile();
+    	    profile.setAlwaysLoadNoFocusLib(true);
+    	    return new FirefoxDriver(profile);
     }
     
     private static WebDriver getPhantomJSDriver() throws IOException{
-      DesiredCapabilities capabilities = DesiredCapabilities.phantomjs();
+        DesiredCapabilities capabilities = DesiredCapabilities.phantomjs();
 
-      ArrayList<String> cliArgsCap = createCliArgs();
-      capabilities.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS, cliArgsCap);
-      capabilities.setCapability(PhantomJSDriverService.PHANTOMJS_PAGE_SETTINGS_PREFIX + "loadImages", true);
-      capabilities.setCapability("takesScreenshot", true);
-      capabilities.setJavascriptEnabled(true);
+        ArrayList<String> cliArgsCap = createCliArgs();
+        capabilities.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS, cliArgsCap);
+        capabilities.setCapability(PhantomJSDriverService.PHANTOMJS_PAGE_SETTINGS_PREFIX + "loadImages", true);
+        capabilities.setCapability("takesScreenshot", true);
+        capabilities.setJavascriptEnabled(true);
      
-      final File phantomjsDriverFile = new File(getAbsolutePath(PHANTOMJS_DRIVER_PATH));
-      if (phantomjsDriverFile.exists() && !phantomjsDriverFile.canExecute()) {
-    	  phantomjsDriverFile.setExecutable(true);
-      }
+        final File phantomjsDriverFile = new File(getAbsolutePath(PHANTOMJS_DRIVER_PATH));
+        if (phantomjsDriverFile.exists() && !phantomjsDriverFile.canExecute()) {
+    	        phantomjsDriverFile.setExecutable(true);
+        }
 
-      capabilities.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, getAbsolutePath(PHANTOMJS_DRIVER_PATH));
+        capabilities.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, getAbsolutePath(PHANTOMJS_DRIVER_PATH));
 
-      return new PhantomJSDriver(capabilities);
-  }
+        return new PhantomJSDriver(capabilities);
+    }
 
-  private static ArrayList<String> createCliArgs() {
-      ArrayList<String> cliArgsCap = new ArrayList<>();
-      cliArgsCap.add("--ignore-ssl-errors=true");
-      cliArgsCap.add("--web-security=false");
-      cliArgsCap.add("--disk-cache=true");
-      cliArgsCap.add("--max-disk-cache-size=256");
-      cliArgsCap.add("--proxy-type=none");
-      return cliArgsCap;
-  }
+    private static ArrayList<String> createCliArgs() {
+        ArrayList<String> cliArgsCap = new ArrayList<>();
+        cliArgsCap.add("--ignore-ssl-errors=true");
+        cliArgsCap.add("--web-security=false");
+        cliArgsCap.add("--disk-cache=true");
+        cliArgsCap.add("--max-disk-cache-size=256");
+        cliArgsCap.add("--proxy-type=none");
+        return cliArgsCap;
+    }
 
 }
